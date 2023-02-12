9a092dc6e083fe1794e17aa8a46386b030eb7e1f
Luis Angel <angel.rh.2108@gmail.com>
2023-02-12 13:30:42 -0600

    REFACTOR[Application]: Implementa el router principal

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-<Layout />
+<router-view />
 </template>
 
 <script>
-import Layout from "@/modules/app/pages/Layout.vue";
 export default
-{
-    components:
-    {
-        Layout: Layout,
-    }
-}
+{}
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,22 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {defineAsyncComponent} from 'vue'
+import {createRouter,createWebHashHistory} from 'vue-router'
 
 const routes = [
   {
+    // Pagina principal
     path: '/',
     name: 'home',
-    component: HomeView
+    component: defineAsyncComponent(() => import(
+      /**LayoutTemplate */
+      "@/modules/app/pages/Layout.vue"))
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(
+      /* webpackChunkName: "about" */
+      '../views/AboutView.vue')
+  },
 ]
 
 const router = createRouter({
