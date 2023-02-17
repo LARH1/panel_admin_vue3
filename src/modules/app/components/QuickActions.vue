dafbab88167197f8427db0e7400911642855818d
Luis Angel <angel.rh.2108@gmail.com>
2023-02-16 21:46:52 -0600

    FEAT[QuickActions]: Base para las acciones rapidas

--- src/modules/app/components/QuickActions.vue
+++ src/modules/app/components/QuickActions.vue
@@ -1,16 +1,16 @@
 <template>
 <li class="nav-item">
-    <a class="nav-link" href="#">
+    <a class="nav-link" href="" @click="action">
         <i class="fa-regular fa-bell"></i>
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="#">
+    <a class="nav-link" href="" @click="action">
         <i class="fa-solid fa-table-cells-large"></i>
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="#">
+    <a class="nav-link" href="" @click="action">
         <i class="fa-regular fa-envelope"></i>
     </a>
 </li>
@@ -20,6 +20,12 @@
 export default
 {
     name: "app-quick-actions",
+    setup()
+    {
+        return {
+            action: () => alert("Algo")
+        }
+    }
 }
 </script>
 
